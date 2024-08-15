e794b020304c16c643845da004d5b5f6673c132a
vokarl <karl.vogl@gmx.de>
2024-08-15 22:29:32 +0200

    added testing for get request in backend

--- backend/src/main/java/org/example/backend/ProductController.java
+++ backend/src/main/java/org/example/backend/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
 
--- backend/src/main/java/org/example/backend/ProductService.java
+++ backend/src/main/java/org/example/backend/ProductService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
