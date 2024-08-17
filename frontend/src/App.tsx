dea173bfeaabaa97b47d42be6f1057b972850527
vokarl <karl.vogl@gmx.de>
2024-08-17 21:18:55 +0200

    add frontend for GetGroceriesById and refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
-
-import './App.css'
-import ProductList from "./componets/ProductList.tsx";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import GetGroceriesById from "./componets/GetGroceriesById";
+import {Product} from "./models/product.tsx";
 
 function App() {
+    const [products, setProducts] = useState<Product[]>([]);
 
-  return (
-      <>
-        <h1>Groceries app</h1>
-        <ProductList/>
+    useEffect(() => {
+        axios.get<Product[]>('api/products')
+            .then(response => {
+                setProducts(response.data);
+            })
+            .catch(error => {
+                console.error("Es gab einen Fehler beim Abrufen der Daten!", error);
+            });
+    }, []);
 
-      </>
-  )
+    return (
+        <>
+            <h1>Groceries app</h1>
+            <GetGroceriesById products={products} />
+
+        </>
+    );
 }
 
-export default App
+export default App;
